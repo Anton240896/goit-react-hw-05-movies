4ed8dcda14229203fc1a08d294636d2d0e472578
Anton240896 <antonalipov791@gmail.com>
2023-10-24 17:51:35 +0300

    preparation

---
+++ src/components/Api/Api.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+/*   ====== FETCH REQUEST API ======*/
+const API_KEY = '64832fedcffbe340ec8c3c22e595e81';
+const ENGLISH_LANG = 'language=en-US';
+const TRENDING = 'trending/movie/day';
+const SEARCH = 'search/movie';
+const MOVIE = 'movie/';
+const CREDITS = '/credits';
+const REVIEWS = '/reviews';
+const PAGE = 'page=1';
+const ADULT_FALSE = 'include_adult=false';
+
+/*   ====== REQUEST API  ======*/
+
+/*   ====== TREND MOVIES FOR TODAY ======*/
+export const requestTrending = async () => {
+  const response = await axios.get(`${TRENDING}?${API_KEY}`);
+  return response.data.results;
+};
+
+/*   ====== SEARCH MOVIES ======*/
+export const requestSearch = async keyword => {
+  const response = await axios.get(
+    `${SEARCH}?${API_KEY}&${ENGLISH_LANG}&${PAGE}&${ADULT_FALSE}&query=${keyword}`
+  );
+  return response.data.results;
+};
+
+/*   ====== MOVIE ======*/
+export const requestMovieDetails = async movieId => {
+  const response = await axios.get(
+    `${MOVIE}${movieId}?${API_KEY}&${ENGLISH_LANG}`
+  );
+  return response.data;
+};
+
+/*   ====== MOVIE ACTORS ======*/
+export const requestActors = async movieId => {
+  const response = await axios.get(
+    `${MOVIE}${movieId}${CREDITS}?${API_KEY}&${ENGLISH_LANG}`
+  );
+  return response.data.cast;
+};
+
+/*   ====== MOVIE REVIEWS ======*/
+export const requestReviews = async movieId => {
+  const response = await axios.get(
+    `${MOVIE}${movieId}${REVIEWS}?${API_KEY}&${ENGLISH_LANG}&${PAGE}`
+  );
+  return response.data.results;
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
+/*   ====== IMPORT ======*/
+
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+
+import { Layout } from 'components/Layout/Layout';
+
+// import Home from 'path/to/pages/Home;';
+// import Movies from 'path/to/pages/Movies';
+// import MovieDetails from 'path/to/pages/MovieDetails';
+// import Cast from 'path/to/pages/Cast';
+// import Reviews from 'path/to/pages/Reviews';
+
+/*   ====== IMPORT-LAZY ======*/
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const NotFound = lazy(() => import('pages/NotFound/NotFound'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
+
+/*   ====== ROUTES ======*/
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
   );
 };

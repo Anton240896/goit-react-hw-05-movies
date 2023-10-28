395a2fb2fb4989668839aaf76ba229e4e748998b
Anton240896 <antonalipov791@gmail.com>
2023-10-28 10:33:06 +0300

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { requestReviews } from 'components/Api/Api';
 import { Loader } from 'components/Loader/Loader';
-import { ListReview } from './Reviews.styled';
+import { ListReview } from 'components/Reviews/Reviews.styled';
 import toast, { Toaster } from 'react-hot-toast';
 
 /*   ====== HOOKS ======*/

1b6496fe19849e5ccff5e8625baa151f9c92de95
Anton240896 <antonalipov791@gmail.com>
2023-10-30 12:18:56 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, Outlet, Link, useLocation } from 'react-router-dom';
+import toast from 'react-hot-toast';
 import { requestMovieDetails } from 'components/Api/Api';
 import Loader from 'components/Loader/Loader';
 import {
@@ -17,31 +18,47 @@ import {
 /*   ====== HOOKS ======*/
 const MovieDetails = () => {
   const { movieId } = useParams();
+  const [error, setError] = useState(false);
   const [movieInfo, setMovieInfo] = useState(null);
   const [loading, setLoading] = useState(false);
   const location = useLocation();
 
   /*   ====== FETCH REQUEST ======*/
   useEffect(() => {
-    const getMovieDetailsFilms = () => {
-      setLoading(true);
-
-      requestMovieDetails(movieId)
-        .then(detailMovie => {
-          setMovieInfo(detailMovie);
-        })
-        .catch(error => {})
-        .finally(() => {
-          setLoading(false);
-        });
+    const getMovieDetailsFilms = async () => {
+      try {
+        setError(false);
+        setLoading(true);
+        const data = await requestMovieDetails(movieId);
+        setMovieInfo(data);
+      } catch (error) {
+        toast.error('Sorry, we didnt find anything');
+      } finally {
+        setLoading(false);
+      }
     };
-
     getMovieDetailsFilms();
   }, [movieId]);
 
   if (!movieInfo) {
     return;
   }
+  // useEffect(() => {
+  //   const getMovieDetailsFilms = () => {
+  //     setLoading(true);
+
+  //     requestMovieDetails(movieId)
+  //       .then(detailMovie => {
+  //         setMovieInfo(detailMovie);
+  //       })
+  //       .catch(error => {})
+  //       .finally(() => {
+  //         setLoading(false);
+  //       });
+  //   };
+
+  //   getMovieDetailsFilms();
+  // }, [movieId]);
 
   /*   ====== DESTRUCTURIZATION  ======*/
   const defaultNoImages =
@@ -108,6 +125,7 @@ const MovieDetails = () => {
           </Link>
 
           {loading && <Loader />}
+          {error && toast.error(`Sorry,we didnt find anything`)}
         </Container>
       }
     </main>

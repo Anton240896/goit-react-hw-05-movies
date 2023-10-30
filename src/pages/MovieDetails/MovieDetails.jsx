b958093131ea59e5c0c4e72f1cc353e503c03db0
Anton240896 <antonalipov791@gmail.com>
2023-10-30 22:05:32 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useParams, Outlet, Link, useLocation } from 'react-router-dom';
-import { HiOutlineArrowLeft } from 'react-icons/hi';
 import toast from 'react-hot-toast';
 import { requestMovieDetails } from 'components/Api/Api';
 import Loader from 'components/Loader/Loader';
@@ -133,9 +132,7 @@ const MovieDetails = () => {
           </ContMovieDetails>
 
           <Link to={location.state?.from ?? '/'}>
-            <ButtonLink type="button">
-              <HiOutlineArrowLeft size={50} />
-            </ButtonLink>
+            <ButtonLink type="button">Go back</ButtonLink>
           </Link>
 
           {loading && <Loader />}

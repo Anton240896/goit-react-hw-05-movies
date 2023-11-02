54baf6e380b52085e916bc6d0aca9eb329477f08
Anton240896 <antonalipov791@gmail.com>
2023-11-02 08:58:42 +0200

    Update ListFilms.styled.js

--- src/components/ListFilms/ListFilms.styled.js
+++ src/components/ListFilms/ListFilms.styled.js
@@ -8,30 +8,9 @@ export const List = styled.ul`
   gap: 10px;
 `;
 
-export const Item = styled.li`
-  font-weight: 600;
-  font-size: 18px;
-  margin-bottom: 10px;
-
-  &:hover {
-    transform: scale(1.03);
-    transition: transform 550ms cubic-bezier(0.4, 0, 0.2, 1);
-  }
-`;
-
-export const MovieLink = styled(Link)`
-  text-decoration: none;
-  color: #191d1e;
-
-  &:hover {
-    color: red;
-  }
-`;
 export const NameFilms = styled.p`
   position: absolute;
-
   width: 10%;
-
   font-size: 20px;
   color: white;
   font-weight: 500;
@@ -41,7 +20,28 @@ export const NameFilms = styled.p`
   overflow: hidden;
   text-overflow: ellipsis;
   background-color: #ff000096;
+`;
+
+export const Item = styled.li`
+  font-weight: 600;
+  font-size: 18px;
+  margin-bottom: 10px;
 
   &:hover {
+    transform: scale(1.03);
+    transition: transform 550ms cubic-bezier(0.4, 0, 0.2, 1);
+
+    & ${NameFilms} {
+      bottom: -200%;
+    }
+  }
+`;
+
+export const MovieLink = styled(Link)`
+  text-decoration: none;
+  color: #191d1e;
+
+  &:hover {
+    color: red;
   }
 `;

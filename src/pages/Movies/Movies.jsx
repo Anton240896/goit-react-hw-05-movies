f56878580a2aea158ec6d8948b6e3c6dc5d315d1
Anton240896 <antonalipov791@gmail.com>
2023-12-14 22:57:42 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -58,9 +58,7 @@ const Movies = () => {
 
       <ListFilms films={movies} />
 
-      {(movies.length > 0 && <ListFilms films={movies} />) || (
-        <NotFilms>No films, enter your request</NotFilms>
-      )}
+      {movies.length === 0 && <NotFilms>No films, enter your request</NotFilms>}
     </div>
   );
 };

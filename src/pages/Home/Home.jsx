d7d1ddd34a014e8c3a0a4f6745b47c6d57391994
Anton240896 <antonalipov791@gmail.com>
2023-12-27 11:58:02 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
   return (
     <ContFilms>
       <Description>
-        Welcome to Movie DB description : <Span>Plot</Span>
+        Welcome to Movie DB description • <Span>Plot</Span>
         <Span>Actors</Span>
         <Span>Reviews</Span>
       </Description>

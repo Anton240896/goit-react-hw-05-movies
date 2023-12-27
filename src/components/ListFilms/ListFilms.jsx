6a3b93db15e7664fc222c6519d056efe75c06752
Anton240896 <antonalipov791@gmail.com>
2023-12-27 12:08:53 +0200

    Update ListFilms.jsx

--- src/components/ListFilms/ListFilms.jsx
+++ src/components/ListFilms/ListFilms.jsx
@@ -1,16 +1,16 @@
 import { useLocation } from 'react-router-dom';
 import { List, Item, MovieLink, NameFilms } from './ListFilms.styled';
 
-//     DESTRUCTURIZATION
+/*   ====== DESTRUCTURIZATION  ======*/
 const defaultImg =
   'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg';
 const posterFilms = 'https://image.tmdb.org/t/p/w500';
 
-//     HOOKS
+/*   ====== HOOKS ======*/
 const ListFilms = ({ films }) => {
   const location = useLocation();
 
-  //   RENDER
+  /*   ====== RENDER ======*/
   return (
     <section>
       <List>

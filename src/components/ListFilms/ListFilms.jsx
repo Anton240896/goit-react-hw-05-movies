8964856769e748b21737b096fb82b7e4a2f921b6
Anton240896 <antonalipov791@gmail.com>
2023-12-27 12:05:57 +0200

    Update ListFilms.jsx

--- src/components/ListFilms/ListFilms.jsx
+++ src/components/ListFilms/ListFilms.jsx
@@ -3,7 +3,7 @@ import { List, Item, MovieLink, NameFilms } from './ListFilms.styled';
 
 const defaultImg =
   'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg';
-const posterPath = 'https://image.tmdb.org/t/p/w500';
+const posterFilms = 'https://image.tmdb.org/t/p/w500';
 
 const ListFilms = ({ films }) => {
   const location = useLocation();
@@ -18,7 +18,7 @@ const ListFilms = ({ films }) => {
                 {title}, {release_date.slice(0, 4)}
               </NameFilms>
               <img
-                src={poster_path ? `${posterPath}${poster_path}` : defaultImg}
+                src={poster_path ? `${posterFilms}${poster_path}` : defaultImg}
                 alt={title}
                 width="150px"
                 height="220px"

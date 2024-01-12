ded14b987574af57b1b0e9795c03abf8c89b7038
Anton240896 <antonalipov791@gmail.com>
2024-01-12 19:00:04 +0200

    delete globalstyle

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -14,15 +14,8 @@ export const GlobalStyles = createGlobalStyle`
 }
 
 body {
-
   margin: 0;
-background-image: radial-gradient(circle, #dbdbe3 60%, rgb(0, 0, 0)75%);
- 
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-image: radial-gradient(circle, #dbdbe3 60%, rgb(0, 0, 0)75%);
 }
 
 code {

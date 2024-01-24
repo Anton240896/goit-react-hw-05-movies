da67e0e888d08a049e7ad4176b389fe5a7ebed9a
Anton240896 <antonalipov791@gmail.com>
2024-01-24 18:25:21 +0200

    Update Trailer.styled.js

--- src/components/Trailer/Trailer.styled.js
+++ src/components/Trailer/Trailer.styled.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 // import { FaYoutube } from 'react-icons/fa';
 
 export const ContTrailer = styled.div`
-  margin-top: 21px;
-  margin-left: -437px;
-  margin-right: -269px;
+  position: relative;
+  top: 40px;
+  left: -390px;
 
   @media (max-width: 1220px) {
     margin-top: 0;
@@ -13,9 +13,10 @@ export const ContTrailer = styled.div`
 `;
 
 export const Frame = styled.iframe`
-  width: 417.5px;
+  width: 320px;
   height: 100px;
   border: thick double var(--tmdbLightGreen);
+  border-radius: 10px;
 
   @media (max-width: 1220px) {
     width: 325px;
